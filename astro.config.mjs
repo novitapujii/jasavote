d3489616eb71f50cccef0c53f76e81385c8f6fe6
novitapujii <novitapuji055@gmail.com>
2026-05-17 00:39:11 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jasavote.my.id', // <-- WAJIB TAMBAHKAN BARIS INI
   integrations: [sitemap()]
 });
